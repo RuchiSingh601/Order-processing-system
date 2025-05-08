636b0c8c72c59302ad8cd1c04570f59fcc0753ec
Nirved Soni <89785704+nirvedsoni@users.noreply.github.com>
2025-05-08 17:02:47 +0530

    Paymentmethod  error fixxed

--- app/Models/Paymentmethod.php
+++ app/Models/Paymentmethod.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Paymentmethod extends Model
+class PaymentMethod extends Model
 {
+    protected $table = 'paymentmethods';
     protected $fillable = ['name', 'status'];
 }
